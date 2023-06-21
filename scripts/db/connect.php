8f0f1ff414788bcfe88e804c4936f2a4eca86c2c
VickyMontanezCampus <vmontanez707@gmail.com>
2023-06-21 09:10:18 -0500

    feat: :construction: First part of a connection with a server
    First part of connection with a server of mysql

---
+++ scripts/db/connect.php
@@ -0,0 +1,21 @@
+<?php
+    interface enviroments
+    {
+        public function __get($name);
+    }
+
+    abstract class connect extends credentials implements enviroments
+    {
+        use getInstance;
+        protected $conx;
+        function __construct(private $driver ='mysql', private $port = 3306){
+            try {
+                $this->conx = new PDO($this->driver.":host=".$this->__get('host').";port=".$this->port.";dbname=".$this->__get('dbname').";user=".$this->user.";password=".$this->password);
+                $this->conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (\PDOExeption $e) {
+                $this->conx = $e->getMessage();
+            }
+        }
+    }
+
+?>  
---
+++ scripts/db/credentials.php
@@ -0,0 +1,8 @@
+<?php
+    abstract class credentials{
+        protected $host ='172.16.48.204';
+        private $user = 'sputnik';
+        private $password = 'Sp3tn1kC@';
+        protected $dbname = 'prueba_camp';
+    }
+?>
